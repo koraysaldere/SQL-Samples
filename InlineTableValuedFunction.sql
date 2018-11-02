57e0f27198b85d1ab8a5a8031d4ae034c49aaaee
Koray Saldere <42040766+koraysaldere@users.noreply.github.com>
2018-11-02 09:23:26 +0300

    Create InlineTableValuedFunction.sql

---
+++ InlineTableValuedFunction.sql
@@ -0,0 +1,14 @@
+create function dbo.GetClients (
+    @clientName nvarchar(max) = null
+)
+returns table
+return 
+
+Select * From
+(
+    select *
+    from dbo.Clients as a
+    where ((a.ClientName = @clientName) or a.ClientName is null)
+)
+as tablename
+
