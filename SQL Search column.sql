a81aeccd049a1496efae37f08fe864f6a063f435
Koray Saldere <42040766+koraysaldere@users.noreply.github.com>
2018-11-12 10:02:02 +0300

    Update SQL Search column.sql

--- SQL Search column.sql
+++ SQL Search column.sql
@@ -1,7 +1,20 @@
 
+-- Search Table
+
 SELECT  OBJECT_NAME([object_id]) AS tableName
 FROM    sys.columns
 WHERE   [name] = 'column';
 
 
 Select * From INFORMATION_SCHEMA.columns Where Column_name Like '%Column%'
+
+
+-- Search Table Valued Function / Store Proc.
+
+SELECT 
+    o.name AS ReferencingObject, 
+    sd.referenced_entity_name AS ReferencedObject
+FROM sys.sql_expression_dependencies  AS sd
+INNER JOIN sys.objects AS o
+    ON o.object_id = sd.referencing_id
+WHERE sd.referenced_entity_name Like '%new3%';
