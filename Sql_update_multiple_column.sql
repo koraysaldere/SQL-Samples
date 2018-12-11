7aa297143cb4f3480b8b80bb5dcba0393f1d0fc0
Koray Saldere <42040766+koraysaldere@users.noreply.github.com>
2018-12-11 09:47:37 +0300

    Create Sql_update_multiple_column.sql

---
+++ Sql_update_multiple_column.sql
@@ -0,0 +1,16 @@
+
+	UPDATE table_name
+
+	SET  table_name.column_name = UPPER (column_name)
+	
+	FROM which_table
+
+	-- Multiple Column and Cond.
+	
+	WHERE table_name.column_name = which_table.column_name 
+  
+  AND table_name_1.column_name_1 = which_table_1.column_name_1 
+  
+  AND table_name_2.column_name_2 = which_table_2.column_name_2 
+  
+  -- If you want add more
