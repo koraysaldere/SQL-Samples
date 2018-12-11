9259940ff823bc49d8899da571c280fc47a9695e
Koray Saldere <42040766+koraysaldere@users.noreply.github.com>
2018-12-11 09:48:33 +0300

    Update and rename Sql_update_multiple_column.sql to Sql update multiple column.sql

--- Sql update multiple column.sql
+++ Sql update multiple column.sql
@@ -9,8 +9,8 @@
 	
 	WHERE table_name.column_name = which_table.column_name 
   
-  AND table_name_1.column_name_1 = which_table_1.column_name_1 
+  	AND table_name_1.column_name_1 = which_table_1.column_name_1 
   
-  AND table_name_2.column_name_2 = which_table_2.column_name_2 
+  	AND table_name_2.column_name_2 = which_table_2.column_name_2 
   
-  -- If you want add more
+  	-- If you want add more
