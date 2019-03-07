39ee47c3e2892194403a3b3aa5117c5ad1a520eb
Koray Saldere <42040766+koraysaldere@users.noreply.github.com>
2019-03-07 23:16:55 +0200

    Update Create View.sql

--- Create View.sql
+++ Create View.sql
@@ -1,5 +1,5 @@
 
---VIEW OLUÞTURMA 
+--VIEW OLUÃžTURMA 
 
 Create View PersonelSatis As 
 Select Per.PersonelID, Adi, SoyAdi, Satis.SatisID, Satis.SatisTarihi, Satis.NakliyeUcreti  From Personeller as Per Inner Join Satislar as Satis
@@ -10,4 +10,32 @@ Per.PersonelID = Satis.PersonelID
 --VIEW NORMAL TABLO OLARAK CAGIRILABILIYOR
 
 Select * From PersonelSatis as Per Where Adi = 'Michael' Order By Per.NakliyeUcreti Desc
+
+
+
+
+
+
+  @Then("^(?:I )?see with escape character (.*)$")
+    public void test1(String str) {
+
+
+
+        System.out.println(str);
+
+    }
+    @Given("^(?:I )?see number (\\d+)$")
+
+    public void test2(int nmbr){
+
+
+
+
+
+    }
+    @Given("^(?:I )?see (\\d+(?:\\.\\d+)?)$")
+    public void test3(int nmbr1) {
+        System.out.println(nmbr1);
+
+    }
 
